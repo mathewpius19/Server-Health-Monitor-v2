a7ee50d444af4aa085c102a90ded3264d59784f6
mathew pius <mathewpius4232@gmail.com>
2020-10-01 17:53:01 +0300

    Added addserver and removeserver routes in posts.js

--- Back-End/src/routes/post.js
+++ Back-End/src/routes/post.js
@@ -57,47 +57,81 @@ router.post("/signin", ({body:{username,password}},res)=>{
         })
     
 });
-router.post("/Add_Server", (req,res)=>{
-   console.log(req.body);
-//    request.post({
-//        url:"http://127.0.0.1/Add_Server",
-//        json:{
-//           IPAddress:req.body.IPAddress,
-//           Servername:req.body.Servername,
-//            Username:req.body.Username
+router.post("/addserver", ({body:{username,user,serverName,sshKey,password,ipAddr}},res)=>{
+   //console.log(req.body);
+    User.findOne({username:username}).exec((err,result)=>{
+        if(err){
+            res.json({message:err});
+            throw(err)           
+        }
+        else{
+            if(result){
+                if(result.servers.length===0){
+                    result.servers.push({
+                        user:user,
+                        serverName:serverName,
+                        sshKey:sshKey,
+                        password:password,
+                        ipAddr:ipAddr
+                    })
+                    result.save();
+                    res.send("New Server Created!")
+                }
+                else{
+                    const nameNotAvailable = (result.servers).map((el)=>el.serverName===serverName).some((el)=>el===true)
+                    //returns an array with same size of resultant.servers containing boolean values and .some() checks if any value inside the array is true.If any value is true then nameNotAvailable gets the value true
+                    if(nameNotAvailable){
+                        res.send("Server already exists. Did not add server");
+                    }
+                    else{
+                        result.servers.push({
+                            user:user,
+                            serverName:serverName,
+                            sshKey:sshKey,
+                            ipAddr:ipAddr,
+                            password:password
+                        });
+                        result.save();
+                        res.send("Added server to list of existing servers");
+                    }
+                    }
+            }
+            else{
+                res.send("User does not exist.Please check username again!");
+            }
+        }
+    }
 
-//        },
-//        headers:{
-//            'Content-type':'application/json'
-//         }
-//    },
-//    function (err,response,body){
-//    if(err){
-//        res.json({message:err})
-//    }
-//    res.send(body);
-// });
+    )
 });
-router.post("/Remove_Server", (req,res)=>{
-//    console.log(req.body);
-//    request.post({
-//        url:"http://127.0.0.1/Remove_Server",
-//        json:{
-//         Username:req.body.Username,
-//         Servername:req.body.Servername
-       
-
-//        },
-//        headers:{
-//            'Content-type':'application/json'
-//         }
-//    },
-//    function (err,response,body){
-//    if(err){
-//        res.json({message:err})
-//    }
-//    res.send(body);
-// });
+router.post("/removeserver", ({body:{username,serverName}},res)=>{
+    User.findOne({username:username}).exec((err,result)=>{
+        if(err){
+            res.json({message:err})
+            throw err
+        }
+        else{
+                 if(result){
+                const serverExists = result.servers
+                .map((el)=>el.serverName===serverName)
+                .some((el)=>el);//returns a boolean value true if serverName matches the inputed serverName
+                if(serverExists){
+                    const serverIdx = result.servers
+                    .map((el)=>el.serverName===serverName)
+                    .indexOf(true);//find the index where value is true/1
+                    result.servers.splice(serverIdx,1);//This removes 1 element from the index which is equal to serverIdx
+                    result.save()
+                    res.send(`Server with name ${serverName} has been removed from list of servers`);
+                }
+                else{
+                    res.send("Server does not exist");
+                }
+            }
+            else{
+                res.send("User does not exist. Please check username again!")
+            }
+        }
+    })
 });
 
 router.post("/Display", (req,res)=>{
